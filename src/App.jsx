042a658e7eb362bc94d5237b44272803f5ea76f4
Rahullchaudharyy <rahullchaudharryy@gmail.com>
2024-09-27 14:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://img.freepik.com/premium-vector/new-soft-pastel-blur-gradient-background_92086-57.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+      className="flex justify-center items-center"
+    >
+      <div className='w-[90vmin] mb-[50vmin]  h-[120vmin] md:w-[100vmin] md:h-[60vmin] bg-red-300 flex items-center justify-center'>
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
